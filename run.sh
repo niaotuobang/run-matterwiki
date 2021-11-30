adea1d8a7adb715c5332598023d80aa75b437701
alingse <alingse@foxmail.com>
2021-12-01 00:44:20 +0800

    add chmod

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p ./db
+mkdir -p ./etc
 docker run --name=matterwiki-qun -p 127.0.0.1:8050:5000 -v $(pwd)/db:/server/db -v $(pwd)/etc/prod.config.js:/server/config.js --restart=always -d matterwiki
