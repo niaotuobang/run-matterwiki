7752a4543a6701abe8b9b8400a733b576595701c
alingse <alingse@foxmail.com>
2021-12-01 00:42:23 +0800

    add run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ./db
+docker run --name=matterwiki-qun -p 127.0.0.1:8050:5000 -v $(pwd)/db:/server/db -v $(pwd)/etc/prod.config.js:/server/config.js --restart=always -d matterwiki
