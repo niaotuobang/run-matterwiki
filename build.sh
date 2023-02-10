411afa1d430ad15e582b365a95fa677190f0b40e
alingse <alingse@foxmail.com>
2023-02-11 00:41:44 +0800

    add new

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone http://github.com/matterwiki/matterwiki
-cd matterwiki
-docker build -t matterwiki .
--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mkdir -p ./db
-mkdir -p ./etc
-docker run --name=matterwiki-qun -p 127.0.0.1:8050:5000 -v $(pwd)/db:/server/db -v $(pwd)/etc/prod.config.js:/server/config.js --restart=always -d matterwiki
+docker run --name=matterwiki-qun -p 127.0.0.1:8001:5000 -v $(pwd)/db:/server/db -v $(pwd)/config.js:/server/config.js --restart=always -d matterwiki
